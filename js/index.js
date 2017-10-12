b343775604bb358d8a2dfeaf8a774f06aee2572e
Default <Default>
2017-10-12 19:03:33 -0400

    added js functions to change iframe
    added js functions to change iframe

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function load_containers(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/containers.html"
+};
+
+function load_paper(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/paper.html"
+};
+
+function load_cardboard(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/cardboard.html"
+};
+
+function load_special(){
+	document.getElementById("recycling_map").src = "http://web.engr.oregonstate.edu/~leame/Interactive_Map/special.html"
+};
